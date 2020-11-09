9d434479d75a3a8dd7a72070743eec4a6faa0b2a
Kevin Vu <kevin.mkh.vu@gmail.com>
2020-11-09 01:30:48 +0100

    switching API

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -16,22 +16,20 @@ function get_location(){
 
   try{
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET', 'http://www.geoplugin.net/json.gp?jsoncallback', false);
+    xmlhttp.open('GET', 'https://ipapi.co/json/', false);
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4) {
             if(xmlhttp.status == 200) {
                 var data= JSON.parse(xmlhttp.responseText);
                 b["ip"] = data["geoplugin_request"].slice(0,55);
-                b["status"] = data["geoplugin_status"];
-                b["delay"] = parseInt(data["geoplugin_delay"].replace("ms",""));
-                b["tz"] = data["geoplugin_timezone"].slice(0,30);
-                b["continent"] = data["geoplugin_continentName"].slice(0,15);
-                b["country"] = data["geoplugin_countryName"].slice(0,50);
-                b["region"] = data["geoplugin_region"].slice(0,50);
-                b["region2"] = data["geoplugin_regionName"].slice(0,50);
-                b["city"] = data["geoplugin_city"].slice(0,50);
-                b["latitude"] = parseFloat(data["geoplugin_latitude"]);
-                b["longitude"] = parseFloat(data["geoplugin_longitude"]);
+                b["postal"] = data["postal"].slice(0,15);
+                b["tz"] = data["timezone"].slice(0,30);
+                b["continent"] = data["continent_code"].slice(0,5);
+                b["country"] = data["country_name"].slice(0,50);
+                b["region"] = data["region"].slice(0,50);
+                b["city"] = data["city"].slice(0,50);
+                b["latitude"] = parseFloat(data["latitude"]);
+                b["longitude"] = parseFloat(data["longitude"]);
              }
         }
     };
@@ -39,8 +37,7 @@ function get_location(){
 
   }
   catch (error){
-    b["IP"] ="error"
-    b["status"] = 0;
+    b["ip"] ="error";
   }
   return b;
 }
