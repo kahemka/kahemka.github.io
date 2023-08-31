c42e902dcbafae91bc6b19aea9258a7e35f7f492
Kevin Vu <kevin.mkh.vu@gmail.com>
2023-08-31 11:56:59 +0200

    Update link

--- static/js/placement/data/pIndex.js
+++ static/js/placement/data/pIndex.js
@@ -3,7 +3,7 @@
     placeMenuButton("menuButton", menuEx)
     
     // Honeycomb menu Placement
-    let honeyEx = [{"title":"Docs","desc":"Personal documentation","img":"static/img/book.JPG","link":"Docs.html"},
+    let honeyEx = [{"title":"Docs","desc":"Personal documentation","img":"static/img/book.JPG","link":"docs.html"},
                    {"title":"3D Exp","desc":"Gallery: 3D modelling experimentation","img":"static/img/3d.jpg","link":"#"},
                    {"title":"IArt","desc":"Gallery: Generative AI","img":"static/img/iart.JPG","link":"#"},
                    {"title":"Bots","desc":"Serverless games/bots","img":"static/img/game.JPG","link":"#"},
